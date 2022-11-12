a8675e75e10b68ccf2e47030a048a654f6146b1f
mrathena <china.mrathena@gmail.com>
2022-11-13 01:34:12 +0800

    .

---
+++ 测试.配件识别.全部.01.截图.py
@@ -0,0 +1,30 @@
+import time
+import mss
+import pynput
+import winsound
+
+
+def mouse():
+
+    sct = mss.mss()
+
+    def grab(region):
+        left, top, width, height = region
+        return sct.grab(monitor={'left': left, 'top': top, 'width': width, 'height': height})
+
+    def down(x, y, button, pressed):
+        if pressed:
+            # 鼠标, 侧上键:结束, 侧下键:识别
+            if button == pynput.mouse.Button.x2:
+                return False
+            elif button == pynput.mouse.Button.x1:
+                # 截图, 3440×1440
+                img = grab((2212, 125, 632, 577))
+                mss.tools.to_png(img.rgb, img.size, output=f'image/3440.1440/weapon.attachment/all/{int(time.time())}.png')
+                winsound.Beep(800, 200)
+
+    with pynput.mouse.Listener(on_click=down) as m:
+        m.join()
+
+
+mouse()
